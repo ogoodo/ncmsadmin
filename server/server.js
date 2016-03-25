03f7a3e2a3c2b756fcafaef002c65f4324a0568e
cxb <chenxb172@outlook.com>
2016-03-25 17:33:36 +0800

    c

--- server/server.js
+++ server/server.js
@@ -5,7 +5,46 @@ var webpack = require('webpack')
 //var webpackDevMiddleware = require('webpack-dev-middleware')
 var app = express()
 var logger = require('morgan');
+var fs=require("fs");
 
+var onFinished = require('on-finished')
+app.use(logger('dev'));
+app.use(function(req, res, next){
+    // console.log('服务器重写:', req.url );
+    if(req.url.indexOf('.')===-1){
+        req.url = '/index.html'
+        //req.originalUrl = '/index.html'
+    }
+    next();
+    // //404后处理, 要编译成本地文件才行
+    // if(!res.hasOwnProperty('statusCode') && !res.finished ){
+    //     console.log('服务器重写2:', res.hasOwnProperty('statusCode'), res.statusCode, ' url:', req.url );
+    //     try{
+    //         var fff = path.join(process.cwd(), './build/index.html')
+    //         var file = fs.readFileSync(fff, "utf8")
+    //         res.send(file)
+    //     }catch(err){
+    //         console.error('server.js', err)
+    //     }
+    // }
+});
+//url重写支持http://127.0.0.1:3001/page1/tab2这种类型加载
+//app.use(rewrite(/(^\/(\w+))+/, '/index.html'));
+app.use(rewrite('/page1', '/index.html'))
+
+app.use(express.static(path.join(__dirname, '../build')))
+
+// app.get('*', function(req, res){
+//     res.render('404.html', {
+//         title: 'No Found'
+//     })
+// });
+// app.use(function catchError(req, res, next, err) {
+//     console.error('Caught error', err);
+//     res.status(500).json({
+//         error: err
+//     });
+// });
 const isDevelopment = function () {
     return process.env.NODE_ENV ? process.env.NODE_ENV.trim()==='development' : false;
     //return process.env.NODE_ENV ? process.env.NODE_ENV.trim()==='production' : false;
@@ -39,29 +78,6 @@ if(isDevelopment()){
     app.use(require('webpack-hot-middleware')(compiler));
     console.log('调试服务器插件启动}}')
 }
-
-app.use(logger('dev'));
-app.use(function(req, res, next){
-    //console.log('网址访问'); //要运行下gulp 
-    // if (req.url.slice(-1) === '/') {
-    //     req.url = req.url.slice(0, -1);
-    // }
-    // debugger
-    // console.log('服务器重写:', req.url );
-    // req.url = '/index.html';
-    // req.req.url = '/index.html';
-    // req.req.originalUrl = '/index.html';
-    next();
-    // console.log('服务器重写2:', req.url );
-    //debugger;
-});
-//url重写支持http://127.0.0.1:3001/page1/tab2这种类型加载
-//app.use(rewrite(/(^\/(\w+))+/, '/index.html'));
-app.use(rewrite('/page1', '/index.html'))
-
-app.use(express.static(path.join(__dirname, '../build')))
-
-
 app.listen(3001, function () {
   console.log('Server listening on http://localhost:3001, Ctrl+C to stop')
 })
