5c327e8f9e5ba57d7fdafeb8b7e7ca4ff099f4dc
chen <chen@chendeMacBook-Pro.local>
2016-07-13 17:37:07 +0800

    漏掉文件

---
+++ src/components/test/Page3.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { browserHistory, Router, Route, Link } from 'react-router'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+
+
+class Page3 extends React.Component {
+  render() {
+    const { pathname } = this.props.location
+
+    return (
+      <div className="Image">
+        <h1>Page 3</h1>
+        <ul>
+          <li><Link to="/page2/tab1">Tab 1</Link></li>
+          <li><Link to="/page2/tab2">Tab 2</Link></li>
+        </ul>
+        <ReactCSSTransitionGroup
+          component="div" transitionName="example"
+          transitionEnterTimeout={500} transitionLeaveTimeout={500}
+        >
+          {React.cloneElement(this.props.children || <div />, { key: pathname })}
+        </ReactCSSTransitionGroup>
+      </div>
+    )
+  }
+}
+Page3.propTypes = {
+    location: React.PropTypes.object.isRequired,
+    children: React.PropTypes.object
+}
+module.exports = Page3
+//export default Page1
