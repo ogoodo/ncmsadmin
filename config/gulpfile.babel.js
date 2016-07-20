0e0276d7fe9d28f8a0e2043ea4a302c149c6e6eb
chen <ogoodo.com@qq.com>
2016-07-20 18:40:59 +0800

    gulp让用户选择编译版本

--- config/gulpfile.babel.js
+++ config/gulpfile.babel.js
@@ -1,38 +1,73 @@
 import path from 'path'
+import fs from 'fs'
+import readline from 'readline'
 import gulp from 'gulp'
 import gutil from 'gulp-util'
 import watch from 'gulp-watch'
 import babel from 'gulp-babel'
 import replace from 'gulp-replace'
+import gulpif from 'gulp-if'
 import webpack from 'webpack'
 import jsdoc from 'gulp-jsdoc3'
 import WebpackDevServer from 'webpack-dev-server'
-var fs = require('fs')
 
-const ROOT_PATH = path.join(process.cwd(), '..')
+var isDll = false
+var isBuild = false
 
-// import webpackConfigProd from './config/webpack.prod.1.config.js';
-// import webpackConfigDev from './config/webpack.dev.1.config.js';
-//import webpackConfigProd from './config/webpack.config.js';
-//import webpackConfig  from './config/webpack.config.js'
+if(false) {
+    console.info('gulp运行目录: ', process.cwd())
+    console.info('gulp运行参数: ', process.argv)
+    const argv = process.argv
+    argv.forEach(function (item) {
+        if(item.indexOf('--NODE_ENV=')===0) {
+            const nodeenv = item.replace('--NODE_ENV=', '')
+            console.info('gulp打包版本: ', nodeenv)
+            process.env.NODE_ENV = nodeenv;
+        }
+    })
+}
 
-//console.log(webpackConfigDev)
-
-// console.warn('gulp********** process.env.NODE_ENV=', process.env.NODE_ENV );
-// const isDevelopment = function () {
-//     return process.env.NODE_ENV ? process.env.NODE_ENV.trim()==='development' : false;
-// };
-
-console.info('gulp运行目录: ', process.cwd())
-console.info('gulp运行参数: ', process.argv)
-const argv = process.argv
-argv.forEach(function (item) {
-    if(item.indexOf('--NODE_ENV=')===0) {
-        const nodeenv = item.replace('--NODE_ENV=', '')
-        console.info('gulp打包版本: ', nodeenv)
-        process.env.NODE_ENV = nodeenv;
-    }
-})
+gulp.task('user.select', (callback) => {
+    const info = `
+    l: 生成dll
+    d: 开发版
+    s: 测试版
+    p: 生产版
+    请选择你想编译的版本(退出ctrl＋c)
+    `.substr(1)
+    console.log(info)
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    })
+    rl.on('line', (line) => {
+        line = line.trim()
+        console.log(`您选择了: ${line}`)
+        if(['l','d','s','p'].indexOf(line) === -1) {
+            console.log('请选择正确的版本')
+        } else {
+            rl.close()
+            switch(line) {
+                case 'l':
+                    isDll = true
+                    break
+                case 'd':
+                    isBuild = true
+                    process.env.NODE_ENV = 'development'
+                    break
+                case 's':
+                    isBuild = true
+                    process.env.NODE_ENV = 'development'
+                    break
+                case 'p':
+                    isBuild = true
+                    process.env.NODE_ENV = 'production'
+                    break
+            }
+            callback(); // 如果 err 不是 null 和 undefined，流程会被结束掉，后面的任务不会被执行
+        }
+    })
+});
 /**
  * 如果js文件格式不合要求生成jsdoc的时候会报错误(esprima), 但是又不会提出什么错误
  * jsdoc估计是基于AST的, 如果js有错误， 他生成就会报错:ogoodo.com:2016.3.30
@@ -48,14 +83,14 @@ gulp.task('make:jsdoc', function () {
     //.pipe(jsdoc('./doc-output'))
 })
 
-gulp.task('html', function () {  
+gulp.task('html', ['user.select'], function () {  
     return gulp
     .src(['../src/template/*.html'])
-    .pipe(gulp.dest('../build/'))
+    .pipe(gulpif(!isDll, gulp.dest('../build/')))
 });
 function getAllFiles() {
     var doc = '';
-    var files = fs.readdirSync('../build/dist/dll/');
+    const files = fs.readdirSync('../build/dist/dll/');
     files.forEach(function(item) {
         //if(item.indexOf('.js')>=0) {
         if(/^dll\..+\.js$/g.test(item)) {
@@ -65,12 +100,16 @@ function getAllFiles() {
     }, this);
     return doc;
 }
-gulp.task('do.dll.ejs.template', function () {
-    var filenames = getAllFiles();
-    return gulp
-    .src(['../src/template/*.ejs'])
-    .pipe(replace(/<!--dll.js.file.replace-->/g, filenames))    
-    .pipe(gulp.dest('../build/template'));
+gulp.task('do.dll.ejs.template', ['user.select'], function () {
+    if(isDll) {
+        return;
+    } else {
+        const filenames = getAllFiles();
+        return gulp
+        .src(['../src/template/*.ejs'])
+        .pipe(gulpif(!isDll, replace(/<!--dll.js.file.replace-->/g, filenames))) 
+        .pipe(gulp.dest('../build/template'))
+    }
 });
 
 // transform
@@ -90,22 +129,29 @@ gulp.task('watch-transform', () => {
     .pipe(gulp.dest('lib'));
 });
 
-gulp.task('webpack:build', ['do.dll.ejs.template'], (callback) => {
-  const webpackConfig  = require('../config/webpack.config.js')
-  // modify some webpack config options
+gulp.task('webpack:dll', ['do.dll.ejs.template'], (callback) => {
+  if( ! isDll) {
+      return;
+  }
+  const webpackConfig  = require('../config/webpack.config.dll.js')
   var myConfig = Object.create( webpackConfig);
-//   myConfig.plugins = myConfig.plugins.concat(
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         // This has effect on the react lib size
-//         'NODE_ENV': JSON.stringify('production')
-//       }
-//     }),
-//     new webpack.optimize.DedupePlugin(),
-//     new webpack.optimize.UglifyJsPlugin()
-//   );
+    webpack(myConfig, (err, stats) => {
+        if (err){
+            throw new gutil.PluginError('webpack:build', err);
+        }
+        gutil.log('[webpack:build]', stats.toString({
+            colors: true
+        }));
+        callback();
+    });
+});
 
-  // run webpack
+gulp.task('webpack:build', ['do.dll.ejs.template'], (callback) => {
+  if( ! isBuild) {
+      return;
+  }
+  const webpackConfig  = require('../config/webpack.config.js')
+  var myConfig = Object.create( webpackConfig);
     webpack(myConfig, (err, stats) => {
         if (err){
             throw new gutil.PluginError('webpack:build', err);
@@ -118,11 +164,14 @@ gulp.task('webpack:build', ['do.dll.ejs.template'], (callback) => {
 });
 
 gulp.task('webpack-dev-server', (callback) => {
-  const webpackConfig  = require('../config/webpack.config.js')
+  const webpackConfig  = isDll ?
+    require('../config/webpack.config.dll.js') :
+    require('../config/webpack.config.js')
+    console.log(webpackConfig)
   // modify some webpack config options
   var myConfig = Object.create(webpackConfig);
-  myConfig.devtool = 'eval';
-  myConfig.debug = true;
+   myConfig.devtool = 'eval';
+   myConfig.debug = true;
 
   // Start a webpack-dev-server
   new WebpackDevServer(webpack(myConfig), {
@@ -140,8 +189,49 @@ gulp.task('webpack-dev-server', (callback) => {
 gulp.task('default', ['watch-transform', 'webpack-dev-server']);
 
 // 执行 gulp prod 打包到dist目录， 部署直接部署dist目录即可
-gulp.task('prod', ['html', 'do.dll.ejs.template', 'webpack:build']);//, 'transform', 'watch-transform'
+gulp.task('prod', ['user.select', 'html', 'do.dll.ejs.template', 'webpack:dll', 'webpack:build']);//, 'transform', 'watch-transform'
 // gulp.task('prod', ['html', 'webpack:build']);//, 'transform', 'watch-transform'
 
 // 生成jsdoc帮助文档
 gulp.task('jsdoc', ['make:jsdoc'])
+
+
+
+// function readSyncByfs(tips) {
+//     var response;
+
+//     tips = tips || '> ';
+//     process.stdout.write(tips);
+//     process.stdin.pause();
+//     response = fs.readSync(process.stdin.fd, 1000, 0, 'utf8');
+//     process.stdin.end();
+//     return response[0].trim();
+// }
+// console.log(readSyncByfs('请输入任意字符：'));
+
+// const ROOT_PATH = path.join(process.cwd(), '..')
+// var ttt = 3
+// const buf = new Buffer(50)
+// function readSyn() {
+//     // process.stdin.resume()
+//     process.stdin.pause()
+//     const res = fs.readSync(process.stdin.fd, buf, 10, 0, 'utf-8')
+//     process.stdin.resume()
+//     console.log(buf, res)
+//     // return res[0].trim()
+// }
+// console.log('请选择: ');
+// console.log('你选择了: ', readSyn());
+
+// return;
+// import webpackConfigProd from './config/webpack.prod.1.config.js';
+// import webpackConfigDev from './config/webpack.dev.1.config.js';
+//import webpackConfigProd from './config/webpack.config.js';
+//import webpackConfig  from './config/webpack.config.js'
+
+//console.log(webpackConfigDev)
+
+// console.warn('gulp********** process.env.NODE_ENV=', process.env.NODE_ENV );
+// const isDevelopment = function () {
+//     return process.env.NODE_ENV ? process.env.NODE_ENV.trim()==='development' : false;
+// };
